940997acb083f24c5ec28e068332684176f2c0b3
Arohan Dutt <arohandutt@gmail.com>
2021-03-13 22:54:58 -0500

    Formatting

--- pages/store-item-gallery-layout/components/gallery/gallery.tsx
+++ pages/store-item-gallery-layout/components/gallery/gallery.tsx
@@ -1,24 +1,18 @@
-import styles from './gallery.module.scss'
-import React from 'react'
+import styles from "./gallery.module.scss";
+import React from "react";
 
-type GalleryProps = (
-    {
-        srcArray?: string[];
-        componentArray?: never;
-    } | {
-        componentArray: React.ReactNode[];
-        srcArray?: never;
+type GalleryProps =
+  | {
+      srcArray?: string[];
+      componentArray?: never;
     }
-)
+  | {
+      componentArray: React.ReactNode[];
+      srcArray?: never;
+    };
 
-const Gallery:React.FC<GalleryProps> = ({componentArray, srcArray}) => {
-
-    console.log(props)
-
-    return (
-        <div>Gallery
-        </div>
-    )
-}
+const Gallery: React.FC<GalleryProps> = ({ componentArray, srcArray }) => {
+  return <div>Gallery</div>;
+};
 
 export default Gallery;
